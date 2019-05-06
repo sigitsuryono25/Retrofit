9deab9865fe5d20f14a1d8b7941da125981416d0
Sigit Suryono <sigitharsy25@gmail.com>
2019-05-06 10:23:01 +0700

    Create Response.kt

---
+++ Response.kt
@@ -0,0 +1,11 @@
+//Response.kt
+
+import javax.annotation.Generated
+import com.google.gson.annotations.SerializedName
+
+@Generated("com.robohorse.robopojogenerator")
+data class Response(
+
+	@field:SerializedName("data")
+	val data: MutableList<DataItem?>? = null
+)
